fd48c3ce8f69191d1ce0d3d4c3eff33f4018fa2e
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-06-03 01:10:30 -0300

    feat: ensure SignUp Controller return 500 if addAccount throws

---
+++ src/presentation/controllers/signUp/signup-controller.ts
@@ -0,0 +1,24 @@
+import { type AddAccount } from '../../../domain/usecases/add-account.use-case'
+import {
+  type Controller,
+  type HttpRequest
+  // type HttpResponse
+} from './signup-controller.protocols'
+
+export class SignUpController implements Controller {
+  constructor (private readonly addAccount: AddAccount) {}
+
+  async handle (httpRequest: HttpRequest): Promise<any> {
+    try {
+      const { name, email, password } = httpRequest.body
+      await this.addAccount.add({
+        name,
+        email,
+        password
+      })
+      return new Promise(() => (null))
+    } catch (error) {
+      return new Error()
+    }
+  }
+}
